1aa4ec7d2a81a4cc967ae1a2efc4dcb3f7c68cd9
Samrat Man Singh <samratmansingh@gmail.com>
2013-01-23 21:53:31 +0545

    Add recode functions

--- src/think_stats/survey.clj
+++ src/think_stats/survey.clj
@@ -30,6 +30,26 @@
             (conj v (make-record line [["caseid" 1 12 int]])))
           [] (clojure.string/split-lines (read-file path))))
 
+(defn- recode-agepreg [pregnancies]
+  (reduce (fn [v preg]
+            (if (not (= (get preg "agepreg") "NA"))
+              (conj v (assoc preg "agepreg" (/ (get preg "agepreg") 100)))
+              (conj v (assoc preg "agepreg" "NA"))))
+          [] pregnancies))
+
+(defn- recode-birthwgt-oz [pregnancies]
+  (reduce (fn [v preg]
+            (let [birthwgt_lb (get preg "birthwgt_lb")
+                  birthwgt_oz (get preg "birthwgt_oz")]
+              (if (and (not (= birthwgt_lb "NA"))
+                       (< birthwgt_lb 20)
+                       (not (= birthwgt_oz "NA"))
+                       (<= birthwgt_oz 16))
+                (conj v (assoc preg "totalwgt_oz"
+                               (+ (* birthwgt_lb 16) birthwgt_oz)))
+                (conj v (assoc preg "totalwgt_oz" "NA")))))
+          [] pregnancies))
+
 (defn pregnancies
   [path]
   (-> (reduce (fn [v line]
@@ -43,4 +63,7 @@
                                            ["birthord" 278 279 int]
                                            ["agepreg" 284 287 int]
                                            ["finalwgt" 423 440 float]])))
-              [] (clojure.string/split-lines (read-file path)))))
+              [] (clojure.string/split-lines (read-file path)))
+      (recode-agepreg)
+      (recode-birthwgt-oz)
+      ))
