f4991ff57e67d0eff26e301a6c601792f230e3f5
Samrat Man Singh <samratmansingh@gmail.com>
2013-01-27 21:00:26 +0545

    Add visualization corresponding to Figure 2-3(Difference in percentage of gestation periods)

--- src/think_stats/descriptive.clj
+++ src/think_stats/descriptive.clj
@@ -11,10 +11,12 @@
                        (not= (get % "birthord") 1)))
                pregnancies)))
 
-(defn plot-frequencies [path]
-  (let [first-born (gestation-period-by-birthord (pregnancies path) true)
-        others (gestation-period-by-birthord (pregnancies path) false)
-        first-hist (into (sorted-map) (hist first-born))
+(def path "2002FemPreg.dat.gz")
+(def first-born (gestation-period-by-birthord (pregnancies path) true))
+(def others (gestation-period-by-birthord (pregnancies path) false))  
+
+(defn plot-frequencies []
+  (let [first-hist (into (sorted-map) (hist first-born))
         others-hist (into (sorted-map) (hist others))]
     (save (bar-chart (interleave (keys first-hist)
                                  (keys others-hist))
@@ -24,18 +26,16 @@
                      :y-label "frequency"
                      :legend true
                      :group-by (take (count (interleave
-                                                      (keys first-hist)
-                                                      (keys others-hist)))
-                                              (interleave
-                                               (repeat "first born")
-                                               (repeat "others"))))
-          "/tmp/foo2.png"
+                                             (keys first-hist)
+                                             (keys others-hist)))
+                                     (interleave
+                                      (repeat "first born")
+                                      (repeat "others"))))
+          "/tmp/freq.png"
           :width 1000)))
 
-(defn plot-pmf [path]
-  (let [first-born (gestation-period-by-birthord (pregnancies path) true)
-        others (gestation-period-by-birthord (pregnancies path) false)
-        first-pmf (into (sorted-map) (pmf (hist first-born)))
+(defn plot-pmf []
+  (let [first-pmf (into (sorted-map) (pmf (hist first-born)))
         others-pmf (into (sorted-map) (pmf (hist others)))]
     (save (bar-chart (interleave (keys first-pmf)
                                  (keys others-pmf))
@@ -45,10 +45,24 @@
                      :y-label "probability"
                      :legend true
                      :group-by (take (count (interleave
-                                                      (keys first-pmf)
-                                                      (keys others-pmf)))
-                                              (interleave
-                                               (repeat "first born")
-                                               (repeat "others"))))
+                                             (keys first-pmf)
+                                             (keys others-pmf)))
+                                     (interleave
+                                      (repeat "first born")
+                                      (repeat "others"))))
           "/tmp/pmf.png"
+          :width 1000)))
+
+(defn plot-pmf-differences []
+  (let [first-pmf (into (sorted-map) (pmf (hist first-born)))
+        others-pmf (into (sorted-map) (pmf (hist others)))
+        weeks (range 35 46)]
+    (save (bar-chart weeks
+                     (map
+                      #(* 100 (- (get first-pmf %)
+                                 (get others-pmf %)))
+                      weeks)
+                     :x-label "weeks"
+                     :y-label (str "100x(PMF(first) - PMF(second))"))
+          "/tmp/bar.png"
           :width 1000)))
